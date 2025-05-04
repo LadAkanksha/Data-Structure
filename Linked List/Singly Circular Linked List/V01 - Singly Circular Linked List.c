ec80d7855dd30c90babfd8a570615ac883c0c323
LadAkanksha <akankshalad2003@gmail.com>
2025-05-04 22:39:38 +0530

    Singly Circular Linked List

---
+++ Linked List/Singly Circular Linked List/V01 - Singly Circular Linked List.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int cnt = 0;
+struct node{
+    int Data;
+    struct node *Next;
+};
+
+void Insert_First(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+
+    NewN = (struct node *)malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+        NewN -> Next = *First;
+    }
+    else
+    {
+        struct node *Temp = *First;
+
+        NewN -> Next = *First;
+        while(Temp -> Next != *First)
+        {
+            Temp = Temp -> Next;
+        }
+        Temp -> Next = NewN;
+        *First = NewN;
+    }
+    printf("\n%d is Successfully Inserted at First.",no);
+    cnt++;
+}
+
+void Display_LL(struct node *First)
+{
+    struct node *Temp = First;
+    if(Temp == NULL)
+    {
+        printf("\nList is Empty...");
+    }
+    else
+    {
+        printf("\n\nElements in Linked List ->\n\t");
+       while(Temp -> Next != First)
+        {
+            printf(" | %d | -> ",Temp -> Data);
+            Temp = Temp -> Next;
+        }
+        printf(" | %d | ",Temp -> Data);
+    }
+
+}
+int main()
+{
+    struct node *Head = NULL;
+
+    Display_LL(Head);
+    Insert_First(&Head , 21);
+    Insert_First(&Head , 32);
+    Insert_First(&Head , 99);
+    Display_LL(Head);
+
+    getch();
+    return 0;
+}
