95e52499dbd2dd750f0c00b32c8d57ad75b23fb0
LadAkanksha <akankshalad2003@gmail.com>
2025-05-05 17:07:40 +0530

    Singly Circular Linked List

---
+++ Linked List/Singly Circular Linked List/V04 - Singly Circular Linked List.c
@@ -0,0 +1,194 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int cnt = 0;
+struct node{
+    int Data;
+    struct node *Next;
+};
+
+void Insert_First(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+
+    NewN = (struct node *)malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+        NewN -> Next = *First;
+    }
+    else
+    {
+        struct node *Temp = *First;
+
+        NewN -> Next = *First;
+        while(Temp -> Next != *First)
+        {
+            Temp = Temp -> Next;
+        }
+        Temp -> Next = NewN;
+        *First = NewN;
+    }
+    printf("\n%d is Successfully Inserted at First.",no);
+    cnt++;
+}
+
+void Insert_Last(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+
+    NewN = (struct node *)malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+        NewN -> Next = *First;
+    }
+    else
+    {
+        struct node *Temp = *First;
+
+        NewN -> Next = *First;
+        while(Temp -> Next != *First)
+        {
+            Temp = Temp -> Next;
+        }
+        Temp -> Next = NewN;
+    }
+    printf("\n%d is Successfully Inserted at Last.",no);
+    cnt++;
+}
+
+void Insert_At_Position(struct node **First , int no , int pos)
+{
+    if(pos < 1 || pos > cnt+1)
+    {
+        printf("\nInvalid Position");
+    }
+    else if(pos == 1)
+    {
+        Insert_First(First,no);
+    }
+    else if(pos == cnt+1)
+    {
+        Insert_Last(First , no);
+    }
+    else
+    {
+        struct node *NewN = NULL , *Temp = *First;
+
+        NewN = (struct node *)malloc(sizeof(struct node));
+
+        NewN -> Data = no;
+        NewN -> Next = NULL;
+
+        while(pos > 2)
+        {
+            Temp = Temp -> Next;
+            pos--;
+        }
+
+        NewN -> Next = Temp -> Next;
+        Temp -> Next = NewN;
+        printf("\n%d is Successfully Inserted at Position.",no);
+        cnt++;
+    }
+}
+
+void Delete_First(struct node **First)
+{
+    struct node *Temp = *First;
+
+    if(*First == NULL)
+    {
+        printf("\nList is Already Empty");
+    }
+    else
+    {
+        if(Temp -> Next != *First)
+        {
+            while(Temp -> Next != *First)
+            {
+                Temp = Temp -> Next;
+            }
+            *First = (*First) -> Next;
+            printf("\n%d is Deleted Successfully from First",Temp -> Next -> Data);
+            free(Temp -> Next);
+            Temp -> Next = (*First);
+        }
+        else
+        {
+            *First = Temp -> Next;
+            printf("\n%d is Deleted Successfully from First",Temp -> Data);
+            free(Temp);
+        }
+        cnt--;
+    }
+}
+void Display_LL(struct node *First)
+{
+    struct node *Temp = First;
+    if(Temp == NULL)
+    {
+        printf("\nList is Empty...");
+    }
+    else
+    {
+        printf("\n\nElements in Linked List ->\n\t");
+        while(Temp -> Next != First)
+        {
+            printf(" | %d | -> ",Temp -> Data);
+            Temp = Temp -> Next;
+        }
+        printf(" | %d | ",Temp -> Data);
+    }
+}
+int main()
+{
+    struct node *Head = NULL;
+
+    Display_LL(Head);
+    Insert_First(&Head , 21);
+    Insert_First(&Head , 32);
+    Insert_First(&Head , 99);
+    Display_LL(Head);
+
+    Insert_Last(&Head , 78);
+    Insert_Last(&Head , 5);
+    Insert_Last(&Head , 96);
+    Display_LL(Head);
+
+    Insert_At_Position(&Head , 22 , -5);
+    Insert_At_Position(&Head , 32 , 10);
+    Insert_At_Position(&Head , 40 , 5);
+    Display_LL(Head);
+
+    Delete_First(&Head);
+    Display_LL(Head);
+    Delete_First(&Head);
+    Delete_First(&Head);
+    Display_LL(Head);
+
+    getch();
+    return 0;
+}
