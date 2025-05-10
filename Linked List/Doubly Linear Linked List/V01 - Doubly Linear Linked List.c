544a156c9eaf2d3f98a2f75de878b2818a568dbe
LadAkanksha <akankshalad2003@gmail.com>
2025-05-10 23:25:08 +0530

    Doubly Linear Linked List

---
+++ Linked List/Doubly Linear Linked List/V01 - Doubly Linear Linked List.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int cnt = 0;
+struct node{
+    struct node *Prev;
+    int Data;
+    struct node *Next;
+};
+
+void Insert_First(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+
+    NewN = (struct node *)malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+    NewN -> Prev = NULL;
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+    }
+    else
+    {
+        NewN -> Next = *First;
+        (*First) -> Prev = NewN;
+        *First = NewN;
+    }
+    printf("\n%d is Successfully Inserted at First.",no);
+    cnt++;
+}
+
+void Display_LL(struct node *First)
+{
+    struct node *Temp = First;
+    if(Temp == NULL)
+    {
+        printf("\nList is Empty..");
+    }
+    else
+    {
+        printf("\n\nElements in Linked List ->\n\t");
+       while( Temp != NULL)
+        {
+            printf(" | %d | <==> ",Temp -> Data);
+            Temp = Temp -> Next;
+        }
+        printf(" NULL ");
+    }
+
+}
+int main()
+{
+    struct node *Head = NULL;
+
+    Display_LL(Head);
+    Insert_First(&Head , 21);
+    Insert_First(&Head , 32);
+    Insert_First(&Head , 99);
+    Display_LL(Head);
+
+    getch();
+    return 0;
+}
