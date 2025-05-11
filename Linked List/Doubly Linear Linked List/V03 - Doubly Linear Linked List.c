6bd1d9cb31a4f1c9b1ee3644a27759467700b7a2
LadAkanksha <akankshalad2003@gmail.com>
2025-05-11 22:48:23 +0530

    Doubly Linear Linked List

---
+++ Linked List/Doubly Linear Linked List/V03 - Doubly Linear Linked List.c
@@ -0,0 +1,158 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int cnt = 0;
+struct node{
+    struct node *Prev;
+    int Data;
+    struct node *Next;
+};
+
+void Insert_First(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+
+    NewN = (struct node *)malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+    NewN -> Prev = NULL;
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+    }
+    else
+    {
+        NewN -> Next = *First;
+        (*First) -> Prev = NewN;
+        *First = NewN;
+    }
+    printf("\n%d is Successfully Inserted at First.",NewN->Data);
+    cnt++;
+}
+
+void Insert_Last(struct node **First , int no)
+{
+    struct node *NewN = NULL;
+    NewN = (struct node *) malloc(sizeof(struct node));
+
+    if(NewN == NULL)
+    {
+        printf("\nMalloc Failed....");
+        return;
+    }
+    NewN -> Prev = NULL;
+    NewN -> Data = no;
+    NewN -> Next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = NewN;
+    }
+    else
+    {
+        struct node *Temp = *First;
+        while(Temp -> Next != NULL)
+        {
+            Temp = Temp -> Next;
+        }
+        Temp -> Next = NewN;
+        NewN -> Prev = Temp;
+    }
+    printf("\n%d is Successfully Inserted at Last.",NewN->Data);
+    cnt++;
+}
+
+void Insert_At_Position(struct node **First , int no , int pos)
+{
+    if(pos < 1 || pos > cnt+1)
+    {
+        printf("\nInvalid Position...");
+    }
+    else if(pos == 1)
+    {
+        Insert_First(First,no);
+    }
+    else if(pos == cnt+1)
+    {
+        Insert_Last(First , no);
+    }
+    else
+    {
+        struct node *NewN = NULL , *Temp = *First;
+
+        NewN = (struct node *) malloc(sizeof(struct node));
+        if(NewN == NULL)
+        {
+            printf("\nMalloc Failed...");
+            return;
+        }
+        NewN -> Prev = NULL;
+        NewN -> Data = no;
+        NewN -> Next = NULL;
+
+        while(pos > 2)
+        {
+            Temp = Temp -> Next;
+            pos--;
+        }
+        NewN -> Prev = Temp;
+        NewN -> Next = Temp -> Next;
+        Temp -> Next -> Prev = NewN;
+        Temp -> Next = NewN;
+
+        printf("\n%d is Successfully Inserted at Given Position by IAP.",NewN->Data);
+        cnt++;
+    }
+}
+
+void Display_LL(struct node *First)
+{
+    struct node *Temp = First;
+    if(Temp == NULL)
+    {
+        printf("\nList is Empty..");
+    }
+    else
+    {
+        printf("\n\nElements in Linked List ->\n\t");
+       while( Temp != NULL)
+        {
+            printf(" | %d | <==> ",Temp -> Data);
+            Temp = Temp -> Next;
+        }
+        printf(" NULL ");
+    }
+}
+
+int main()
+{
+    struct node *Head = NULL;
+
+    Display_LL(Head);
+    Insert_First(&Head , 21);
+    Insert_First(&Head , 32);
+    Insert_First(&Head , 99);
+    Display_LL(Head);
+
+    Insert_Last(&Head , 12);
+    Insert_Last(&Head , 45);
+    Insert_Last(&Head , 88);
+    Display_LL(Head);
+
+    Insert_At_Position(&Head , 200,-9);
+    Insert_At_Position(&Head , 300,8);
+    Insert_At_Position(&Head , 400,2);
+    Insert_At_Position(&Head , 500,8);
+    Display_LL(Head);
+
+    getch();
+    return 0;
+}
